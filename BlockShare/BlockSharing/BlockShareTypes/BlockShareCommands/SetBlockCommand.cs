55cab0405bd146ab91fb3045aecf56d73e408725
Dmitry Monakhov <dimamon6032403@gmail.com>
2021-11-11 18:44:09 +0100

    SetBlockCommand implemented

--- BlockShare/BlockSharing/BlockShareTypes/BlockShareCommands/SetBlockCommand.cs
+++ BlockShare/BlockSharing/BlockShareTypes/BlockShareCommands/SetBlockCommand.cs
@@ -10,6 +10,8 @@ namespace BlockShare.BlockSharing.BlockShareTypes.BlockShareCommands
 {
     public class SetBlockCommand : BlockShareCommand
     {
+        public string Path { get; set; }
+        public long BlockIndex { get; set; }
         public byte[] Block { get; set; }
 
         public override BlockShareCommandType CommandType => BlockShareCommandType.SetBlock;
@@ -18,25 +20,36 @@ namespace BlockShare.BlockSharing.BlockShareTypes.BlockShareCommands
         {
         }
 
-        public SetBlockCommand(byte[] block)
+        public SetBlockCommand(string path, long blockIndex, byte[] block)
         {
+            Path = path;
+            BlockIndex = blockIndex;
             Block = block;            
         }
 
         public override void WriteValuesToClient(TcpClient tcpClient, NetStat netStat)
         {
-            NetUtils.WriteBytesFixed(Block, 0, (int)Preferences.BlockSize, tcpClient, netStat);
+            //NetUtils.WriteBytesFixed(Block, 0, (int)Preferences.BlockSize, tcpClient, netStat);
+
+            NetUtils.WriteString(Path, tcpClient, netStat);
+            NetUtils.WriteLong(BlockIndex, tcpClient, netStat);
+            NetUtils.WriteBytes(Block, tcpClient, netStat);
+
             netStat.Payload += (ulong)Preferences.BlockSize;
         }
 
         protected override void ReadValuesFromClient(TcpClient tcpClient, NetStat netStat, long timeout)
         {
-            Block = NetUtils.ReadBytesFixed((int)Preferences.BlockSize, tcpClient, netStat, timeout);
+            //Block = NetUtils.ReadBytesFixed((int)Preferences.BlockSize, tcpClient, netStat, timeout);
+            Path = NetUtils.ReadString(tcpClient, netStat, timeout);
+            BlockIndex = NetUtils.ReadLong(tcpClient, netStat, timeout);
+            Block = NetUtils.ReadBytes(tcpClient, netStat, timeout);
+            netStat.Payload += (ulong)Block.Length;
         }
 
         public override string ToString()
         {
-            return $"SetBlock(Block: {Utils.PrintHex(Block, 0, 16)}...)";
+            return $"SetBlock(Path: {Path}, BlockIndex: {BlockIndex}, Block: {Utils.PrintHex(Block, 0, 16)}...)";
         }
     }
 }
